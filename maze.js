27491c260555cfe76755a4b3161bd71a88043381
Bérengère Carpentier <72983609+carpentierberengere@users.noreply.github.com>
2020-11-23 13:59:48 +0100

    js file

--- maze.js
+++ maze.js
@@ -1,6 +1,13 @@
 const main=document.querySelector("main")
 main.className="sol"
 
-const multiline='***********.**S.....**.*.T*****.....*.******.***.*.******.*****.******.*****.******.......******.********.........****.******...***....********'
+const murMaze=document.createElement("div");
+murMaze.className="mur";
+main.appendChild(murMaze);
 
+const solMaze=document.createElement("div");
+solMaze.className="chemin";
+main.appendChild(solMaze);
+
+const multiline='***********.**S.....**.*.T*****.....*.******.***.*.******.*****.******.*****.******.......******.********.........****.******...***....********'
 
